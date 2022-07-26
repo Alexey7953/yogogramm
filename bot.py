486946ee068a028faddb8e0f8deb0c5a46d1b08b
Alexey Zakharov <zakharov_alexey@mail.ru>
2022-07-26 12:04:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,35 @@
 import config
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import KeyboardButton
+from aiogram.dispatcher.filters import Text
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler
-async def pb():
-    keyboard = types.ReplyKeyboardMarkup()
+# Кнопки
+@dp.message_handler(commands="go")
+async def push_button(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_1 = types.KeyboardButton(text="Кнопка 1")
-    keyboard.add(button_1)
     button_2 = types.KeyboardButton(text="Кнопка 2")
-    keyboard.add(button_2)
+    keyboard.add(button_1, button_2)
+    await message.answer("Какую кнопку выберешь ТЫ?", reply_markup=keyboard)
 
 
+# Ответы на кнопки
+@dp.message_handler(Text(equals="Кнопка 1"))
+async def with_button_one(message: types.Message):
+    await message.reply("Отлично, ты первый!")
+
+
+@dp.message_handler(lambda message: message.text == "Кнопка 2")
+async def with_button_two(message: types.Message):
+    await message.reply("Ничего, ты второй")
+
+
+# Дублирование сообщения
 @dp.message_handler()
 async def bot_answer(message: types.Message):
     await message.answer(message.text)
